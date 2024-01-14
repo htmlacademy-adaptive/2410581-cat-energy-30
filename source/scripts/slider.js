54ff7ac1923fced55a94a7b4342d73a6c60d4710
hRacc00n <MrAlexKr@gmail.com>
2024-01-14 10:40:29 +0300

    Добавляет зеленые полоски

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -3,8 +3,10 @@ const dragLine = document.querySelector('.slider__drag-line');
 const imageBefore = document.querySelector('.slider__image-before');
 const imageAfter = document.querySelector('.slider__image-after');
 
-slider.oninput = () => {
-  dragLine.style.left = `${slider.value}%`;
-  imageBefore.style.clipPath = `inset(0 ${100 - slider.value}% 0 0)`;
-  imageAfter.style.clipPath = `inset(0 0 0 ${slider.value}%)`;
-};
+if (slider) {
+  slider.oninput = () => {
+    dragLine.style.left = `${slider.value}%`;
+    imageBefore.style.clipPath = `inset(0 ${100 - slider.value}% 0 0)`;
+    imageAfter.style.clipPath = `inset(0 0 0 ${slider.value}%)`;
+  };
+}
