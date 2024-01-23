e5a52461fdd4276a013fe4cbdd98d7fa5da75339
hRacc00n <MrAlexKr@gmail.com>
2024-01-23 21:05:58 +0300

    Дорабатывает Pixel Perfect

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,3 +3,26 @@ import '../scripts/nojs';
 import '../scripts/map';
 import '../scripts/burger';
 import '../scripts/slider';
+
+// function getTextNodesIn(elem, opt_fnFilter) {
+//   var textNodes = [];
+//   if (elem) {
+//     for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+//       var node = nodes[i],
+//         nodeType = node.nodeType;
+//       if (nodeType == 3) {
+//         if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+//           if (node.data.trim() !== '') {
+//             textNodes.push(node.data.trim() !== '' ? node : '');
+
+//           }
+//         }
+//       } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+//         textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+//       }
+//     }
+//   }
+//   return textNodes;
+// }
+
+// getTextNodesIn(document.body).forEach((item) => { item.textContent = "енот полоскун полоскает полоску" })
