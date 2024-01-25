7d6d16525d2214f4b486fac84d5aee97e20e60cf
hRacc00n <MrAlexKr@gmail.com>
2024-01-25 20:23:58 +0300

    Чистит код, добавляет переменные

--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,15 +1,4 @@
 const sliderForm = document.querySelector('.slider__container');
-// const dragLine = document.querySelector('.slider__drag-line');
-// const imageBefore = document.querySelector('.slider__image-before');
-// const imageAfter = document.querySelector('.slider__image-after');
-
-// if (slider) {
-//   slider.oninput = () => {
-//     dragLine.style.left = `${slider.value}%`;
-//     imageBefore.style.clipPath = `inset(0 ${100 - slider.value}% 0 0)`;
-//     imageAfter.style.clipPath = `inset(0 0 0 ${slider.value}%)`;
-//   };
-// }
 
 if (sliderForm) {
   const sliderButton = document.querySelector('.slider__button');
